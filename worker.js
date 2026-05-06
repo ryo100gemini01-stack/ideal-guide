5ce1669beedacaede7453fc0daf44949749cea1a
ryo100gemini01-stack <ryo100gemini01@gmail.com>
2026-05-06 12:04:13 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -4,10 +4,21 @@ export default {
 
     const buffer = await res.arrayBuffer()
 
-    // Shift_JISでデコード
-    const text = new TextDecoder("shift-jis").decode(buffer)
+    let text
 
-    const data = JSON.parse(text)
+    try {
+      text = new TextDecoder("shift-jis").decode(buffer)
+    } catch (e) {
+      // ここで落ちてる可能性が高い
+      return new Response("shift-jis decode failed: " + e.message)
+    }
+
+    let data
+    try {
+      data = JSON.parse(text)
+    } catch (e) {
+      return new Response("JSON parse failed: " + e.message + "\n\n" + text.slice(0, 200))
+    }
 
     return Response.json(data)
   }
