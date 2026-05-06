df7d67e597f2781d8054fe66b8beb60d91d39b19
ryo100gemini01-stack <ryo100gemini01@gmail.com>
2026-05-06 12:01:06 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,13 +2,11 @@ export default {
   async fetch() {
     const res = await fetch("https://nico-rank.com/api/ranking?game&period=24h")
 
-    // JSONとして読まない
     const buffer = await res.arrayBuffer()
 
-    // UTF-8としてそのままデコード
-    const text = new TextDecoder("utf-8").decode(buffer)
+    // Shift_JISでデコード
+    const text = new TextDecoder("shift-jis").decode(buffer)
 
-    // ここで初めてJSON.parse
     const data = JSON.parse(text)
 
     return Response.json(data)
